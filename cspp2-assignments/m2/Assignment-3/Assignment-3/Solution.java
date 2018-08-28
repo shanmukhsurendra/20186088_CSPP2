5177f65517df2725a3c0b60f500cfa7780d84b79
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-28 19:17:15 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Assignment-3/Solution.java
@@ -1,35 +1,50 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+    *Do not modify this main function.
+    */
+    private Solution() {
+        /**
+         * { item_description }.
+         */
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      base      The base
+     * @param      exponent  The exponent
+     *
+     * @return     { description_of_the_return_value }
+     */
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	static void baseExpo(final int base, final int exponent) {
-		/**
-		 * { var_description }.
-		 */
-		if (exponent == 0) {
-			System.out.println(1);
-		}
-		if (exponent > 1) {
-		base = base * base;
-		baseExpo(base, exponent - 1);
-	}
-		else {
-			System.out.println(base);
-		}
+    static int power(final int base, final int exponent) {
+        /**
+         * { var_description }.
+         */
+        if (exponent == 0) {
+            return 1;
+        } else {
+            return base * power(base, exponent - 1);
+        }
 
-		
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        int result = power(base, exponent);
+        System.out.println(result);
 
-	}
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int base = s.nextInt();
-		int exponent = s.nextInt();
-		baseExpo(base, exponent);
-		
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
+    }
+    /*
+    Need to write the power function and print the output.
+    */
 }
