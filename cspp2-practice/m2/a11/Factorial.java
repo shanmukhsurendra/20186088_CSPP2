9370d8c545087088a6277b72dcef7f572ff4bfbf
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-28 19:10:49 +0530

    factorial

--- cspp2-practice/m2/a11/Factorial.java
+++ cspp2-practice/m2/a11/Factorial.java
@@ -1,1 +1,43 @@
-public static void main(string[] args)
+import java.util.Scanner;
+/**
+ * { item_description }.
+ */
+public final class Factorial {
+    /**
+     * Constructs the object.
+     */
+    private Factorial() {
+        //fdg.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static int facto(final int n) {
+        /**
+         * { item_description }.
+         */
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * facto(n - 1);
+        }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }
+         */
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int result = facto(n);
+        System.out.println(result);
+    }
+}
