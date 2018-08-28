4fbb4632e50a31eca2348bd970776cc9afb29d3d
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-28 14:37:29 +0530

    areaoftriangle

--- cspp2_assignments/m1/Assignment-3/Assignment-3/Assignment-2/Solution.java
+++ cspp2_assignments/m1/Assignment-3/Assignment-3/Assignment-2/Solution.java
@@ -1,19 +1,49 @@
+/**
+ * { item_description }.
+ */
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      base    The base
+     * @param      height  The height
+     */
+    static void areaOfTriangle(final int base, final int height) {
+        /**
+         * { var_description }.
+         */
+        final float five = 0.5;
+        double area;
+        area = (five * base * height);
+        System.out.println(area);
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int base = scan.nextInt();
-		int height = scan.nextInt();
-		double area;
-		area = (0.5 * base * height);
-		System.out.println(area);
-	}
-	/*
-	Need to fill the areaOfTriangle function and print the output
-	of fahrenheit.
-	*/
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner scan = new Scanner(System.in);
+        int base = scan.nextInt();
+        int height = scan.nextInt();
+        areaOfTriangle(base, height);
+    }
+    /*
+    Need to fill the areaOfTriangle function and print the output
+    of fahrenheit.
+    */
 }
