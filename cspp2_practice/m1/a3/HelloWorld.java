c60d03f9220c0b6999bb7390487d12d99bff204d
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-28 10:14:42 +0530

    helloworld

---
+++ cspp2_practice/m1/a3/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ *hellow world program.
+ */
+public final class HelloWorld {
+    /**
+     *sdfs.
+     */
+    private HelloWorld() {
+        //hgdh.
+    }
+    /**
+     *@param args of type string[].
+     */
+    public static void main(final String[] args) {
+    /**main function.
+    */
+        System.out.println("Hello, World");
+   }
+}
