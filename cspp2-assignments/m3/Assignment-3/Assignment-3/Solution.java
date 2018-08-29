dd35e2c11cc12dbd01a95fd97927b25d80e47a2e
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-29 11:48:39 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m3/Assignment-3/Assignment-3/Solution.java
@@ -0,0 +1,46 @@
+ 
+import java.util.Scanner;
+/*
+	Do not modify this main function.
+	*/
+public class Solution {
+    /**
+     * { function_description }
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static int gcd(final int n1, final int n2) {
+        /**
+         * { item_description }
+         */
+        int result = 1;
+        for (int i=1;i<=n1 && i<=n2;i++){
+            if (n1%i == 0 && n2%i==0){
+                result = i;
+            }
+        }
+        return result;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+
+    public static void main(String[] args) {
+        /**
+         * { var_description }
+         */
+
+        Scanner s=new Scanner(System.in);      
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        System.out.println(gcd(n1,n2));
+    }
+    /*
+	Need to write the gcd function and print the output.
+	*/
+}
