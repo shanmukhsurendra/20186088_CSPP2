25f176a028942c195a7c4902430c20653e794341
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-29 13:36:54 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m3/Assignment-2/Assignment-2/Solution.java
@@ -3,32 +3,38 @@
  */
 import java.util.Scanner;
 /**
-	Do not modify this main function.
-	*/
-public class Solution {
-	/** Fill the main function to print the number of 7's between 1 to n*/
-	public static void main(String[] args) {
-		/**
-		 * { var_description }.
-		 */
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int count = 0;
-		for (int i = 7; i <= n; i++) {
-			int y = i;
-			while (y > 0) {
-				int x = y % 10;
-				if (x == 7) {
-					count = count + 1;
-				}
-				y = y / 10;
-			}
+    Do not modify this main function.
+    */
+public final class Solution {
+    /** Fill the main function to print the number of 7's between 1 to n.
+     */
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int count = 0;
+        for (int i = 7; i <= n; i++) {
+            int y = i;
+            while (y > 0) {
+                int x = y % 10;
+                if (x == 7) {
+                    count = count + 1;
+                }
+                y = y / 10;
+            }
 
 
-		}
-		System.out.println(count);
+        }
+        System.out.println(count);
 
 
 
-	}
+    }
 }
