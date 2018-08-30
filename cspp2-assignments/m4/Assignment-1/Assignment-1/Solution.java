201020775ad67022ffd09308ba62cfadb684d137
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-30 15:41:31 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-1/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Assignment-1/Solution.java
@@ -1,44 +1,46 @@
 /**
  * { item_description }
  */
-import java.util.*;
+import java.util.Scanner;
 /**
  * Class for solution.
  */
 public final class Solution {
-	/**
-	*Fill this main function to print maximum of given array.
-	*/
-	private Solution() {
-		/**
-		 * { item_description }.
-		 */
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		/**
-		 * { var_description }.
-		 */
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int temp = 0;
-		int[] a = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = sc.nextInt();
-		}
-		temp = a[0];
-		for (int j = 0; j < n; j++) {
-			if (temp < a[j]) {
-				temp = a[j];
-			} 
-		}
-		System.out.println(temp);
+    /**
+    *Fill this main function to print maximum of given array.
+    */
+    private Solution() {
+        /**
+         * { item_description }.
+         */
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = 0;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        temp = a[0];
+        for (int j = 0; j < n; j++) {
+            if (temp < a[j]) {
+                temp = a[j];
+            }
+        }
+        System.out.println(temp);
 
 
 
-	}
+    }
 }
+
+
