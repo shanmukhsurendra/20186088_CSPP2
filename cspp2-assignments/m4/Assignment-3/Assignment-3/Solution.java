f3f589894bed3da10dceb4ee364b2d02eab40cf5
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-30 19:14:14 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -1,53 +1,51 @@
 /**
  * { item_description }.
  */
-
-import java.lang.Math;
 import java.util.Scanner;
 /**
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * { function_description }
-	 *
-	 * @param      s     { parameter_description }.
-	 *
-	 * @return     { description_of_the_return_value }.
-	 */
-	static long binaryToDecimal(String s) {
-		/**
-		 * { var_description }.
-		 */
-		long i = (long)Long.parseLong(s);
-		int b = 1;
-		final int ten = 10;
-		long sum = 0;
-		long y = i;
-		while (y > 0) {
-			long x = y % ten;
-			sum += x * Math.pow(2, b-1);
-			y = y / ten;
-			b = b + 1;
-		}
-		return sum;
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments.
-	 */
-	public static void main(String[] args) {
-		/**
-		 * { var_description }.
-		 */
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			long res = binaryToDecimal(s); //Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    static long binaryToDecimal(final String s) {
+        /**
+         * { var_description }.
+         */
+        long i = (long)Long.parseLong(s);
+        int b = 1;
+        final int ten = 10;
+        long sum = 0;
+        long y = i;
+        while (y > 0) {
+            long x = y % ten;
+            sum += x * Math.pow(2, b - 1);
+            y = y / ten;
+            b = b + 1;
+        }
+        return sum;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            long res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
 
 }
