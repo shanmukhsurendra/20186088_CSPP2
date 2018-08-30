22a9784756da0114e899d354c4201e886fe7130e
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-08-30 15:57:17 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -1,44 +1,65 @@
-import java.util.*;
-public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a1srowa = sc.nextInt();
-		int a1colms = sc.nextInt();
-		int[][] a1 = new int[a1srowa][a1colms];
-		for (int i = 0; i < a1srowa; i++) {
-			for (int j = 0; j < a1colms; j++) {
-				a1[i][j] = sc.nextInt();
-			}
-		}
-		int a2srowa = sc.nextInt();
-		int a2colms = sc.nextInt();
-		int[][] a2 = new int[a2srowa][a2colms];
-		for (int i = 0; i < a2srowa; i++) {
-			for (int j = 0; j < a2colms; j++) {
-				a2[i][j] = sc.nextInt();
-			}
-		}
-		int[][] sum = new int[a1srowa][a1colms];
-		if (a1.length == a2.length) {
-			for (int x = 0; x < a1srowa; x++) {
-				for (int y = 0; y < a1colms; y++) {
-					sum[x][y] = a1[x][y] + a2[x][y];
-				}
-			}
-			for (int i = 0; i < a2srowa; i++) {
-			for (int j = 0; j < a2colms; j++) {
-			if (j == a2colms-1) {
-				System.out.println(sum[i][j]);
-			 } 
-			else {
-				System.out.println(sum[i][j] + " ");
-			}
-			}
-			}
+/**
+ * { item_description }.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * { item_description }.
+     */
+    private Solution() {
+        /**
+         * { item_description }.
+         */
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner sc = new Scanner(System.in);
+        int a1srowa = sc.nextInt();
+        int a1colms = sc.nextInt();
+        int[][] a1 = new int[a1srowa][a1colms];
+        for (int i = 0; i < a1srowa; i++) {
+            for (int j = 0; j < a1colms; j++) {
+                a1[i][j] = sc.nextInt();
+            }
+        }
+        int a2srowa = sc.nextInt();
+        int a2colms = sc.nextInt();
+        int[][] a2 = new int[a2srowa][a2colms];
+        for (int i = 0; i < a2srowa; i++) {
+            for (int j = 0; j < a2colms; j++) {
+                a2[i][j] = sc.nextInt();
+            }
+        }
+        int[][] sum = new int[a1srowa][a1colms];
+        if (a1.length == a2.length) {
+            for (int x = 0; x < a1srowa; x++) {
+                for (int y = 0; y < a1colms; y++) {
+                    sum[x][y] = a1[x][y] + a2[x][y];
+                }
+            }
+            for (int i = 0; i < a2srowa; i++) {
+                for (int j = 0; j < a2colms; j++) {
+                    if (j == a2colms - 1) {
+                        System.out.println(sum[i][j]);
+                    } else {
+                        System.out.println(sum[i][j] + " ");
+                    }
+                }
+                System.out.println();
+            }
 
-		} else {
-			System.out.println("not possible");
-		}
-	}
+        } else {
+            System.out.println("not possible");
+        }
+    }
 }
