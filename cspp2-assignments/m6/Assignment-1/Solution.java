392e4b97df0d6eb3909e9bbd04568d04b87eb2de
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-09-01 09:59:00 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -6,43 +6,44 @@ import java.util.Scanner;
  * @author :
  */
 public final class Solution {
-	/**
-	* Empty constructor.
-	*/
-	private Solution() {
-		//not used
-	}
-	/**
-	 * Prints the odd composite numbers between 2 and n.
-	 *
-	 * @param      n     n value
-	 */
-	static void oddComposites(final int n) {
-		
-		for (int i = 2; i <= n; i++) {
-			if (i % 2 != 0) {
-				int count = 0;
-				for (int j = 1; j <= i; j++) {
-					if (i % j == 0) {
-						count = count + 1;
-					}
-				}
-				if (count > 2) {
-					System.out.println(i);
+    /**
+    * Empty constructor.
+    */
+    private Solution() {
+        //not used
+    }
+    /**
+     * Prints the odd composite numbers between 2 and n.
+     *
+     * @param      n     n value
+     */
+    static void oddComposites(final int n) {
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 != 0) {
+                int count = 0;
+                for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) {
+                        count = count + 1;
+                    }
+                }
+                if (count > 2) {
+                    System.out.println(i);
 
-				}
-			}
-		}
+                }
+            }
+        }
 
-	}
-	/**
-	* main method as driver program.
-	* @param args is the parameter for this method
-	*/
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		oddComposites(n);
-	}
+    }
+    /**
+    * main method as driver program.
+    * @param args is the parameter for this method
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        oddComposites(n);
+    }
 }
+
+
 
