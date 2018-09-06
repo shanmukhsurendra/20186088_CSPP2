02ccc789f436ce743d705da8f9d484ece0f0e4a5
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-09-06 17:34:39 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m10/Assignment-3/Assignment-3/Solution.java
@@ -303,7 +303,7 @@ public class Solution {
 					if (tokens.length == 2) {
 						String[] t1 = tokens[1].split(",");
 						Student[] temp = new Student[t1.length];
-						for (int i = 0; i < t1.length ; i++ ) {
+					for (int i = 0 ; i < t1.length ; i++) {
 							temp[i] = new Student(t1[i]);
 						}
 						listStudent.addAll(temp);
