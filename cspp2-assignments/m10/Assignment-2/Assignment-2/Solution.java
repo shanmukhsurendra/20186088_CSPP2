736269bdd841a9826b01c6c000b47c5cf054b2e5
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-09-06 12:13:03 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m10/Assignment-2/Assignment-2/Solution.java
@@ -7,9 +7,14 @@ public class Solution {
      *
      * @param      args  The arguments
      */
-    private Solution(){
+    private Solution() {
         // empty constructor.
     }
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
 
     public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
