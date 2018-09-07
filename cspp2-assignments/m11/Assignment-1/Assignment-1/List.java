f364e8e04a60e6b0ea7af4a31614fb893ef847fb
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-09-07 14:51:53 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-1/Assignment-1/List.java
+++ cspp2-assignments/m11/Assignment-1/Assignment-1/List.java
@@ -341,7 +341,8 @@ public class List {
      */
     public List subList(final int start, final int end) {
 
-        if (start < 0 || end < 0 || start >= end || start + 1 == end) {
+        if (start < 0 || end < 0 || start >= end ||
+         start>size||end> size) {
             System.out.println("Index Out of Bounds Exception");
             return null;
         } else {
