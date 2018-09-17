22fe3b5e176ab77080ab06e20b0802b747964945
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-09-17 19:07:20 +0530

    "check_style_score -> 13 of 13 passed. style score: 0/1 "

---
+++ cspp2-assignments/m19/Assignment-1/Assignment-1/Quiz.java
@@ -0,0 +1,41 @@
+class Quiz {
+    /**
+     * Constructs the object.
+     */
+    Quiz() {
+
+    }
+    /**
+     * variable TEN.
+     */
+    final int TEN = 10;
+    /**
+     * questions array.
+     */
+    String[] questions = new String[TEN];
+    /**
+     *choices array.
+     */
+    String[] choices = new String[TEN];
+    /**
+     * correctAns array.
+     */
+    String[] correctAns = new String[TEN];
+    /**
+     *marks array.
+     */
+    String[] marks = new String[TEN];
+    /**
+     * penality array.
+     */
+    String[] penality = new String[TEN];
+    /**
+     * responses array.
+     */
+    String[] responses = new String[TEN];
+    /**
+     * integer.
+     */
+    int i = 0;
+
+}
