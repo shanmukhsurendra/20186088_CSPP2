a6cde6dcc82d82c3afc22bf7da5368893b505e76
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-12-16 10:42:26 +0530

    "cspp2_m10_a02: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- JAVA PRACTICE/M10/Assignment-2 (1)/Assignment-2/StringList.java
+++ JAVA PRACTICE/M10/Assignment-2 (1)/Assignment-2/StringList.java
@@ -171,7 +171,7 @@ public class StringList implements StringListInterface {
 
 	public void remove(int index) {
 		if (index < 0 || index > size) {
-			System.out.println("Invalid Index Position");
+			System.out.println("Invalid Position Exception");
 		} else {
 			for (int i = 0; i < size-1; i++) {
 				list[i] = list[i + 1];
