cedd506fd44a8c386046ae573f9161c23708500b
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2018-12-22 16:04:45 +0530

    "cspp2_m10_a1_01: 6/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ExamRemedial/CSPP2 Remedial Exam/CSPP2 Remedial Exam/Reservation.java
+++ ExamRemedial/CSPP2 Remedial Exam/CSPP2 Remedial Exam/Reservation.java
@@ -2,47 +2,98 @@
   Reservation class, stores the person and room number in the Hotel
 */
 import java.util.Arrays;
-
+/**
+ * Class for reservation.
+ */
 public class Reservation {
-    //instance variables
+    /**
+     * creates the name.
+     */
     private String name;
+    /**
+     * creates the roonNumber.
+     */
     private int roomNumber;
 
     //constructors, must supply the name, and optionally a room
+
+    /**
+     * Constructs the object.
+     *
+     * @param      person  The person
+     */
     public Reservation(String person) {
         this.name = person;
 
     }
+    /**
+     * Constructs the object.
+     *
+     * @param      person  The person
+     * @param      room    The room
+     */
     public Reservation(String person, int room) {
         this.name = person;
         this.roomNumber = room;
 
     }
 
-    //mutators, set the room number or name
+    /**
+     *  mutators, set the room number or name.
+     */
+
     public void setRoom(int newroom) {
         this.roomNumber = newroom;
     }
-
+    /**
+         *  mutators, set the room number or name.
+         */
     public void setName(String newname) {
         this.name = newname;
     }
 
-    //accessors, return the room number or name
+    /**
+     * Gets the room.
+     *
+     * @return     The room.
+     */
     public int getRoom() {
         return this.roomNumber;
     }
-
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
     public String getName() {
         return this.name;
     }
 }
+/**
+ * Class for hotel.
+ */
 class Hotel {
+    /**
+     * Constructs the object.
+     */
     Hotel() {
 
     }
+    /**
+     * intialises the string array.
+     */
     String[] reservearra = new String[2 + 2 + 1];
+    /**
+     * initialises the size integer.
+     */
     int size = 0;
+    /**
+     * reerves room for a person.
+     *
+     * @param      person  The person
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int reserveRoom(String person) {
         for (int i = 0; i < reservearra.length; i++) {
             if (reservearra[i] != null) {
@@ -56,6 +107,14 @@ class Hotel {
         }
         return -1;
     }
+    /**
+     * reserve particular room for person.
+     *
+     * @param      person   The person
+     * @param      roomNum  The room number
+     *
+     * @return     { description_of_the_return_value }
+     */
     public boolean reserveRoom(String person, int roomNum) {
         if (size < reservearra.length && reservearra[roomNum - 1] != null) {
             System.out.println("Room is already reserved");
@@ -69,6 +128,9 @@ class Hotel {
         size++;
         return true;
     }
+    /**
+     * print the entire reservations.
+     */
     public void printReservations() {
         for (int i = 0; i < reservearra.length; i++) {
             if (reservearra[i] != null) {
@@ -77,14 +139,26 @@ class Hotel {
             }
         }
     }
+    /**
+     * cancel the reservations for particular person.
+     *
+     * @param      person  The person
+     */
     public void cancelReservations(String person) {
-        for( int j = 0 ;j < reservearra.length; j++) {
-            if(reservearra[j].equals(person)) {
+        for ( int j = 0 ; j < reservearra.length; j++) {
+            if (reservearra[j].equals(person)) {
                 reservearra[j] = null;
                 size--;
             }
         }
     }
+    /**
+     * Builds rooms.
+     *
+     * @param      num   The number
+     *
+     * @return     The rooms.
+     */
     public boolean buildRooms(int num) {
         reservearra = Arrays.copyOf(reservearra, reservearra.length * 2);
         return true;
